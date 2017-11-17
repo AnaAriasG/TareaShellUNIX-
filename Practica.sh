d30c795fd8d7a3d75b1f4c56ee0d88ccfe7643e5
Ana <ana.arias@alumnos.unican.es>
2017-11-17 19:24:23 +0100

    preparando fichero para extraer datos

--- Practica.sh
+++ Practica.sh
@@ -1,14 +1,34 @@
-IFS=$'\n'
-for line in $(cat Prov.txt)
+IFS=$'\n' #Pongo un salto de linea como delimitador
+
+#Creo directorios y subdirectorios
+for line in $(cat Prov.txt) 
 do
-	mkdir -p ./$(echo $line| cut -d "," -f 1)"_"$(echo $line| cut -d "," -f 2)/$(echo $line| cut -d "," -f 3)"_"$(echo $line| cut -d "," -f 4) #Creando directorios y subdirectorios
+	mkdir -p ./$(echo $line| cut -d "," -f 1)"_"$(echo $line| cut -d "," -f 2)/$(echo $line| cut -d "," -f 3)"_"$(echo $line| cut -d "," -f 4) 
 done
 
-for file in datos/*
+#Propar0 los ficheros para extraer datos con mayor facilidad
+for anho in {2006..2014}
 do
-	sed "1,9d" $file/T_127P*.txt >  $file/new_T_127P.txt #Eliminar cabecera
+	for mes in Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre
+	do
+		#Comienzo eliminando la cabecera
+		sed "1,9d" datos/$mes"_"$anho/T_127P*.txt >  datos/$mes"_"$anho/new_T_127P*.txt
+		#sustituyo todos los espacios en blanco por uno solo y elimino los puntos que separan los miles
+		tr -s " " < datos/$mes"_"$anho/new_T_127P.txt |tr -d "." > datos/$mes"_"$anho/aux1.txt 
+		#sustituyo los espacios entre los campos por comas teniendo el cuenta la clase de los caracteres.
+		sed -i -e 's/\([A-Z]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g'  -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([A-Z]\)/\1\,\2/g' datos/$mes"_"$anho/aux1.txt
+		sed -i -e 's/SCT/S.C.T/g' datos/$mes"_"$anho/aux1.txt # vuelvo a poner los puntos en las provincias con abreviaciones (S/C Tenerife)
+	done
 done
 
-
-#sed 's/ //g' datos/*/T_127P*.txt > datos/*/T_127P*.txt #eliminar espacios en blanco
-	#sed 's /       / , / g' sample1.txt
+#Lo hago de nuevo todo pero para los unicos meses de los que se tienen datos en 2015
+for mes in Enero Febrero Marzo Abril 
+do
+	#Comienzo eliminando la cabecera
+	sed "1,9d" datos/$mes"_"2015/T_127P*.txt >  datos/$mes"_"2015/new_T_127P*.txt
+	#sustituyo todos los espacios en blanco por uno solo y elimino los puntos que separan los miles
+	tr -s " " < datos/$mes"_"2015/new_T_127P.txt |tr -d "." > datos/$mes"_"2015/aux1.txt 
+	#sustituyo los espacios entre los campos por comas teniendo el cuenta la clase de los caracteres.
+	sed -i -e 's/\([A-Z]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g'  -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([A-Z]\)/\1\,\2/g' datos/$mes"_"2015/aux1.txt
+	sed -i -e 's/SCT/S.C.T/g' datos/$mes"_"2015/aux1.txt # vuelvo a poner los puntos en las provincias con abreviaciones (S/C Tenerife)
+done
