103fc71c1957a32242cae9f74d228c98e9e27976
Ana <ana.arias@alumnos.unican.es>
2017-11-24 12:36:25 +0100

    Arreglando Coruña y comentando scripts

--- Descarga.sh
+++ Descarga.sh
@@ -5,27 +5,30 @@ for anho in {2006..2014}
 do
 	for mes in Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre
 	do	
-		curl -o Datos_$anho'_'$mes.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$anho/$mes'_'$anho.zip		
-		unzip Datos_$anho'_'$mes.zip -d datos/
-		mv datos/$mes datos/$mes'_'$anho #Solo pone el mes
-		mv datos/$mes' '$anho datos/$mes'_'$anho #mes y año separados por un espacio
-		rm Datos_$anho'_'$mes.zip		
+		curl -o Datos_$anho'_'$mes.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/$anho/$mes'_'$anho.zip	#Descargo los archivos
+		unzip Datos_$anho'_'$mes.zip -d datos/ #Dsescomprimo los archivos
+		mv datos/$mes datos/$mes'_'$anho #Aquellos archivos que vienen solo con el nombre del mes, los guardo como "mes_año"
+		mv datos/$mes' '$anho datos/$mes'_'$anho #Aquellos archivos que vienen con mes y año separados por un espacio,  los guardo como "mes_año"
+		rm Datos_$anho'_'$mes.zip		#elimino los archivos comprimidos
 	done
 done
+
+#Los meses correspondientes al año 2009 tiene la ruta diferente, asi que los descargos por separado con este bucle
 for mes2 in Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre
 	do
-		curl -o Datos_2009_$mes2.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2009/$mes2'%20'2009.zip	#En el 2009 cambia la ruta
-		unzip Datos_2009_$mes2.zip -d datos/
-		mv datos/$mes2 datos/$mes2'_'2009 
-		rm Datos_2009_$mes2.zip
+		curl -o Datos_2009_$mes2.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2009/$mes2'%20'2009.zip	#Descargo los archivos. Cambio la ruta
+		unzip Datos_2009_$mes2.zip -d datos/ #Descomprimo los archivos
+		mv datos/$mes2 datos/$mes2'_'2009  #Aquellos archivos que vienen solo con el nombre del mes, los guardo como "mes_año"
+		rm Datos_2009_$mes2.zip #Elimino los archivos comprimidos
 done	
 
+# Solo hay datos de Enero a Abril en el año 2015, asi que los descargo por separado en este bucle
 for mes3 in Enero Febrero Marzo Abril  #solo 4 meses de 2015
 	do	
-		curl -o Datos_2015_$mes3.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2015/$mes3'_'2015.zip		
-		unzip Datos_2015_$mes3.zip -d datos/
-		mv datos/$mes3 datos/$mes3'_'2015
-		rm Datos_2015_$mes3.zip
+		curl -o Datos_2015_$mes3.zip http://www.minetad.gob.es/energia/balances/Publicaciones/ElectricasMensuales/2015/$mes3'_'2015.zip		#Descargo los archivos
+		unzip Datos_2015_$mes3.zip -d datos/ #Descomprimo los archivos
+		mv datos/$mes3 datos/$mes3'_'2015 #Aquellos archivos que vienen solo con el nombre del mes, los guardo como "mes_año"
+		rm Datos_2015_$mes3.zip #Elimino los archivos comprimidos
 done
 
 
--- Practica.sh
+++ Practica.sh
@@ -1,47 +1,61 @@
 IFS=$'\n' #Pongo un salto de linea como delimitador
 
-#Creo directorios y subdirectorios
+#Creo directorios y subdirectorios aprovechando la lista de comunidades autonomas y provincias.
+
 for line in $(cat Prov.txt) 
 do
 	mkdir -p ./$(echo $line| cut -d "," -f 1)"_"$(echo $line| cut -d "," -f 2)/$(echo $line| cut -d "," -f 3)"_"$(echo $line| cut -d "," -f 4) 
 done
 
-#Preparo los ficheros para extraer datos con mayor facilidad
+# Recorro años y meses para preparar los ficheros para la extraccion de datos y procedo a su extraccion y volcado en el fichero correspondiente.
 for anho in {2006..2015}
 do
 	j=0
 	for mes in Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre
 	do
-		if [ $anho == 2015)) ] && [ $mes == Mayo ]; then
+		if [ $anho == 2015 ] && [ $mes == Mayo ]; then # En 2015 solo hay datos hasta el mes de Abril
 			break
 		fi
-		let j+=1
-		#Comienzo eliminando la cabecera
-		sed "1,9d" datos/$mes"_"$anho/T_127P*.txt >  datos/$mes"_"$anho/new_T_127P*.txt
-		#sustituyo todos los espacios en blanco por uno solo y elimino los puntos que separan los miles
-		tr -s " " < datos/$mes"_"$anho/new_T_127P.txt |tr -d "." > datos/$mes"_"$anho/aux1.txt 
-		#sustituyo los espacios entre los campos por comas teniendo el cuenta la clase de los caracteres.
-		sed -i -e 's/\([A-Z]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g'  -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([A-Z]\)/\1\,\2/g' datos/$mes"_"$anho/aux1.txt
-		sed -i -e 's/SCT/S.C.T/g' datos/$mes"_"$anho/aux1.txt # vuelvo a poner los puntos en las provincias con abreviaturas (S/C Tenerife)
+		let j+=1 #Creo un contandor que me facilitará la escritura de la fecha correspondiente a cada dato en cada fichero
+		#Preparo los ficheros para extraer datos con mayor facilidad
+		
+		sed "1,9d" datos/$mes"_"$anho/T_127P*.txt >  datos/$mes"_"$anho/aux1.txt #Comienzo eliminando la cabecera
+		#Sustituyo todos los espacios en blanco por uno solo y elimino los puntos que separan los miles
+		tr -s " " < datos/$mes"_"$anho/aux1.txt |tr -d "." > datos/$mes"_"$anho/aux2.txt 
+		#Sustituyo los espacios entre los campos por comas teniendo el cuenta la clase de los caracteres.
+		sed -i -e 's/\([A-Z]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g'  -e 's/\([0-9]\)\ \([0-9]\)/\1\,\2/g' -e 's/\([0-9]\)\ \([A-Z]\)/\1\,\2/g' datos/$mes"_"$anho/aux2.txt
+		sed -i -e 's/SCT/S.C.T/g' datos/$mes"_"$anho/aux2.txt # Vuelvo a poner los puntos en las provincias con abreviaturas (S/C Tenerife)
 		for  line in $(cat Prov.txt) 
 		do	
-			#Recojo en una variable cada uno de los 4 campos de cada linea
+			#Recojo en una variable cada uno de los 4 campos de cada linea de Prov.txt
 			IDCA=$(echo $line| cut -d "," -f 1)
 			CA=$(echo $line| cut -d "," -f 2)
 			IDP=$(echo $line| cut -d "," -f 3)
 			PRO=$(echo $line| cut -d "," -f 4)
+			
+			
 			if (($j < 10)); then #-lt
 				m=0$j
-			else
+			else                           #Aprovecho el contador j que inicializo arriba para poner la fecha que acompañara a cada dato
 				m=$j
 			fi
+							
 			k=2
 			#A medida que voy leyendo cada linea de Prov.txt creo un fichero dentro de cada provincia para cada tipo de energia
-			for TipEner in Nuclear Carbones Lignitos Fuel GasNatural OtrosCombustibles Total
-			do
-				echo $anho$m $(awk -F "," -v k="$k" -v PRO="$PRO" '$1==PRO{print $k}' datos/$mes"_"$anho/aux1.txt) >> ./$IDCA"_""$CA"/$IDP"_""$PRO"/$TipEner.txt			
-				let k+=1
-			done
+			#Distingo la provincia de Coruña de las demas por la dificultad para reconocer el caranter "ñ" 
+			if [ $PRO == "LA CORUÑA" ];then
+				for TipEner in Nuclear Carbones Lignitos Fuel GasNatural OtrosCombustibles Total
+				do
+					echo $anho$m $(awk -F "," -v k="$k" '$1~"LA CORU"{print $k}' datos/$mes"_"$anho/aux2.txt) >> ./$IDCA"_""$CA"/$IDP"_""$PRO"/$TipEner.txt		#Volcado de cada dato en el fichero correspondiente	
+					let k+=1             																														#acompañado de la fecha	
+				done					
+			else	
+				for TipEner in Nuclear Carbones Lignitos Fuel GasNatural OtrosCombustibles Total
+				do
+					echo $anho$m $(awk -F "," -v k="$k" -v PRO="$PRO" '$1==PRO{print $k}' datos/$mes"_"$anho/aux2.txt) >> ./$IDCA"_""$CA"/$IDP"_""$PRO"/$TipEner.txt			
+					let k+=1
+				done
+			fi
 		done		
 	done
 done
